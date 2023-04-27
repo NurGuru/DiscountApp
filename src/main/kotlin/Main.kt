9cb072720c119166a1bbea4dd2fb49d4f0416d93
Nurgali <117362075+NurGuru@users.noreply.github.com>
2023-04-27 14:46:52 +0300

    second_feature-get_guests_list_by_status1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
+import data.model.Status
+
 fun main() {
     val adapter = Di.createGuestAdapter()
 
     adapter.getGuestByIdAndPrintInfo(1)
     adapter.getGuestByIdAndPrintInfo(2)
     adapter.getGuestByIdAndPrintInfo(3)
+
+    adapter.getGuestsWithStatus(Status.NEW)
+    adapter.getGuestsWithStatus(Status.GOOD)
+    adapter.getGuestsWithStatus(Status.VIP)
 }
